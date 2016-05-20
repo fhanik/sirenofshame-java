bdcf24169ba96ed4c7bfba378be5663a4548e706
Lee Richardson <github@leerichardson.com>
2016-05-19 23:20:16 -0400

    Successfully reading all led and audio patterns

---
+++ src/main/java/com/hanik/usb/siren/LedPattern.java
@@ -0,0 +1,14 @@
+package com.hanik.usb.siren;
+
+import java.util.Arrays;
+
+public class LedPattern {
+    public LedPattern(byte[] pattern) {
+        Id = pattern[0];
+        byte[] newArray = Arrays.copyOfRange(pattern, 1, pattern.length);
+        Name = new String(newArray).trim();
+    }
+
+    public int Id;
+    public String Name;
+}
--- src/main/java/com/hanik/usb/siren/examples/ReadLedPatterns.java
+++ src/main/java/com/hanik/usb/siren/examples/ReadLedPatterns.java
@@ -5,10 +5,7 @@
 
 package com.hanik.usb.siren.examples;
 
-import com.hanik.usb.siren.AudioPattern;
-import com.hanik.usb.siren.JavaxSiren;
-import com.hanik.usb.siren.PacketUtils;
-import com.hanik.usb.siren.SirenControlPacket;
+import com.hanik.usb.siren.*;
 
 import javax.usb.UsbDevice;
 import javax.usb.UsbException;
@@ -18,16 +15,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ReadLedPatterns {
+
+    public static final byte REPORTID_IN_INFO = (byte) 0x01;
+    public static final byte REPORTID_IN_READAUDIOPACKET = (byte) 0x03;
+    public static final byte REPORTID_IN_READLEDPACKET = (byte) 0x04;
+
     public static void main(String[] arg) throws UsbException, InterruptedException {
         UsbHub hub = UsbHostManager.getUsbServices().getRootUsbHub();
         UsbDevice siren = JavaxSiren.findSiren(hub);
         JavaxSiren.claimAndOpenSiren(siren);
 
+        System.out.println("Audio Patterns:");
         List<AudioPattern> audioPatterns = getAudioPatterns(siren);
         for (AudioPattern audioPattern : audioPatterns) {
             System.out.println("\t" + audioPattern.Id + " - " + audioPattern.Name);
         }
+
+        System.out.println("LED Patterns:");
+        List<LedPattern> ledPatterns = getLedPatterns(siren);
+        for (LedPattern ledPattern : ledPatterns) {
+            System.out.println("\t" + ledPattern.Id + " - " + ledPattern.Name);
+        }
         JavaxSiren.release(siren);
+    }
+
+    private static List<LedPattern> getLedPatterns(UsbDevice siren) throws UsbException {
+        ArrayList<LedPattern> ledPatterns = new ArrayList<LedPattern>();
+        int id = 0;
+        while (true) {
+            LedPattern ledPattern = getLedPattern(siren, id);
+            if (ledPattern.Id == -1) break;
+            ledPatterns.add(ledPattern);
+            id++;
+        }
+        return ledPatterns;
+    }
+
+    private static LedPattern getLedPattern(UsbDevice siren, int index) throws UsbException {
+        SirenControlPacket sirenControlPacket = new SirenControlPacket();
+        sirenControlPacket.setReadLedIndex((byte) index);
+        sirenControlPacket.setName("read.led.0");
+        byte[] readData = new byte[21];
+        JavaxSiren.getInputReport(siren, PacketUtils.getControlMessage(sirenControlPacket), readData, REPORTID_IN_READLEDPACKET);
+        return new LedPattern(readData);
     }
 
     private static List<AudioPattern> getAudioPatterns(UsbDevice siren) throws UsbException {
@@ -47,7 +77,7 @@ public class ReadLedPatterns {
         sirenControlPacket.setReadAudioIndex((byte) index);
         sirenControlPacket.setName("read.led.0");
         byte[] readData = new byte[21];
-        JavaxSiren.getInputReport(siren, PacketUtils.getControlMessage(sirenControlPacket), readData, (byte)0x03);
+        JavaxSiren.getInputReport(siren, PacketUtils.getControlMessage(sirenControlPacket), readData, REPORTID_IN_READAUDIOPACKET);
         return new AudioPattern(readData);
     }
 }
